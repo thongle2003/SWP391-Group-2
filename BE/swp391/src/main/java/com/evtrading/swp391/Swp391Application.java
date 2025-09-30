d3b0701b378218387ebb20335a68edc1888ba8dc
besofuls <tnminh170425@gmail.com>
2025-09-30 10:29:00 +0700

    BE folder và môi trường spring boot (3.5.6, java 21)

---
+++ BE/swp391/src/main/java/com/evtrading/swp391/Swp391Application.java
@@ -0,0 +1,13 @@
+package com.evtrading.swp391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391Application.class, args);
+	}
+
+}
