f22bcf90317490b536237248efdca8094bd73928
nguyenbinhphuonghuy0304-stack <nguyenbinhphuonghuy0304@gmail.com>
2025-10-06 13:18:32 +0700

    add profile and fix color page

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import UserProfile from './pages/UserProfile'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<UserProfile />} />
       </Routes>
     </Router>
   )
